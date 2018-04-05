7ad2bfed869fad6ea3ca03c3ab22984714efb119
cpro95 <cpro95@gmail.com>
2018-04-05 16:46:16 +0900

    pagination added with sql limit and offset

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -19,18 +20,15 @@ type Movie struct {
 }
 
 func main() {
+	fmt.Println("Launching MyMovie REST API...........")
 	r := gin.Default()
 	r.Use(Cors())
 
 	v1 := r.Group("api/v1")
 	{
-		// v1.POST("/users", PostUser)
 		v1.GET("/movies", GetMovies)
 		v1.GET("/movies/search", GetMovie)
-		// v1.PUT("/users/:id", UpdateUser)
-		// v1.DELETE("/users/:id", DeleteUser)
 		v1.OPTIONS("/movies", OptionsMovie)
-		v1.OPTIONS("/movies/:id", OptionsMovie)
 	}
 
 	r.Run(":8080")
@@ -42,27 +40,6 @@ func OptionsMovie(c *gin.Context) {
 	c.Next()
 }
 
-// func PostUser(c *gin.Context) {
-// 	db := InitDb()
-// 	defer db.Close()
-
-// 	var user Users
-// 	c.Bind(&user)
-
-// 	if user.Firstname != "" && user.Lastname != "" {
-// 		// INSERT INTO "users" (name) VALUES (user.Name);
-// 		db.Create(&user)
-
-// 		// Display status
-// 		c.JSON(201, gin.H{"success": user})
-// 	} else {
-// 		// Display error
-// 		c.JSON(422, gin.H{"error": "Fields are empty"})
-// 	}
-
-// 	// curl -i -X POST -H "Content-Type: application/json" -d "{\"firstname\":\"Thea\",\"lastname\":\"Queen\" }" http://localhost:8080/api/v1/users
-// }
-
 func GetMovies(c *gin.Context) {
 	// Connection to the database
 	db := InitDb()
@@ -73,14 +50,16 @@ func GetMovies(c *gin.Context) {
 	var movies []Movie
 	var movie Movie
 
-	// // SELECT * FROM movie
-	// // db.Table("movie").Where("c00 like ?", "%interstellar%").Select("idMovie, c00, c01, c03, c05, c07, c08, c20, c21").Limit(10).Find(&movies)
-	// db.Table("movie").Select("idFile, c00, c01, c03, c05, c07, c08, c20, c21").Find(&movies)
-	// //db.Table("movie").Limit(10).Find(&movies)
-	// fmt.Println(movies)
+	// Query string parameters are parsed using the existing underlying request object.
+	// The request responds to a url matching:  /search?id=123&name=string
+	// c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+	limitQuery := c.Request.URL.Query().Get("limit")
+	offsetQuery := c.Request.URL.Query().Get("offset")
+	// fmt.Println("id : " + limitQuery)
+	// fmt.Println("name : " + offsetQuery)
 
 	// query
-	rows, err := db.Query("SELECT idMovie, c00, c01, c03, c05, c07, c08, c20, c21 FROM movie")
+	rows, err := db.Query("SELECT idMovie, c00, c01, c03, c05, c07, c08, c20, c21 FROM movie order by idMovie asc limit ? offset ?", limitQuery, offsetQuery)
 	if err != nil {
 		panic(err)
 	}
@@ -165,91 +144,6 @@ func GetMovie(c *gin.Context) {
 
 	// curl -i http://localhost:8080/api/v1/users/1
 }
-
-// func UpdateUser(c *gin.Context) {
-// 	// Connection to the database
-// 	db := InitDb()
-// 	// Close connection database
-// 	defer db.Close()
-
-// 	// Get id user
-// 	id := c.Params.ByName("id")
-// 	var user Users
-// 	// SELECT * FROM users WHERE id = 1;
-// 	db.First(&user, id)
-
-// 	if user.Firstname != "" && user.Lastname != "" {
-// 		if user.Id != 0 {
-// 			var newUser Users
-// 			c.Bind(&newUser)
-
-// 			result := Users{
-// 				Id:        user.Id,
-// 				Firstname: newUser.Firstname,
-// 				Lastname:  newUser.Lastname,
-// 			}
-// 			// UPDATE users SET firstname='newUser.Firstname', lastname='newUser.Lastname' WHERE id = user.Id;
-// 			db.Save(&result)
-
-// 			// Display modified data in JSON message "success"
-// 			c.JSON(200, gin.H{"success": result})
-// 		} else {
-// 			// Display JSON error
-// 			c.JSON(404, gin.H{"error": "User not found"})
-// 		}
-
-// 	} else {
-// 		// Display JSON error
-// 		c.JSON(422, gin.H{"error": "Fields are empty"})
-// 	}
-
-// 	// curl -i -X PUT -H "Content-Type: application/json" -d "{ \"firstname\":\"Thea\", \"lastname\": \"Merlyn\" }" http://localhost:8080/api/v1/users/1
-// }
-
-// func DeleteUser(c *gin.Context) {
-// 	// Connection to the database
-// 	db := InitDb()
-
-// 	// Close connection database
-// 	defer db.Close()
-
-// 	// Get id user
-// 	id := c.Params.ByName("id")
-// 	var user Users
-// 	// SELECT * FROM users WHERE id = 1;
-// 	db.First(&user, id)
-
-// 	if user.Id != 0 {
-// 		// DELETE FROM users WHERE id = user.Id
-// 		db.Delete(&user)
-// 		// Display JSON result
-// 		c.JSON(200, gin.H{"success": "User #" + id + " deleted"})
-// 	} else {
-// 		// Display JSON error
-// 		c.JSON(404, gin.H{"error": "User not found"})
-// 	}
-
-// 	// curl -i -X DELETE http://localhost:8080/api/v1/users/1
-// }
-
-// func InitDb() *gorm.DB {
-// 	// Openning file
-// 	db, err := gorm.Open("sqlite3", "./MyVideos99.db")
-// 	db.LogMode(true)
-
-// 	// Error
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	// Creating the table
-// 	// if !db.HasTable(&Users{}) {
-// 	// 	db.CreateTable(&Users{})
-// 	// 	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&Users{})
-// 	//
-
-// 	return db
-// }
 
 func InitDb() *sql.DB {
 	// Openning file
